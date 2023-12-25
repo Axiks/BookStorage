3e72a62719706eecb7d7153e43d797f1144c30af
Neko <36519646+Axiks@users.noreply.github.com>
2023-12-25 16:20:14 +0100

    create db context and book entity model

---
+++ BookStorage.Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using BookStorage.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace BookStorage.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        DbSet<BookEntity> Books { get; set; }
+    }
+}
---
+++ BookStorage.Data/Entities/BookEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStorage.Data.Entities
+{
+    public class BookEntity
+    {
+        public Guid Id {  get; set; }
+        public required string Name { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+}
