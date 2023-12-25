2d394167745f117c4c88693edd1bf36c2d2989a0
Neko <36519646+Axiks@users.noreply.github.com>
2023-12-25 16:06:03 +0100

    decelerate SwaggerResponse type

--- BookStorage.API/Controllers/StorageController.cs
+++ BookStorage.API/Controllers/StorageController.cs
@@ -1,6 +1,8 @@
 ﻿using BookStorage.Core.Interfaces;
 using BookStorage.Core.Request;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,9 +12,16 @@ namespace BookStorage.API.Controllers
     [ApiController]
     public class StorageController : ControllerBase
     {
-        private IBookStorageService _bookStorageService;
+        private readonly IBookStorageService _bookStorageService;
+
+        public StorageController(IBookStorageService bookStorageService)
+        {
+            _bookStorageService = bookStorageService;
+        }
+
         // GET: api/<StorageController>
         [HttpGet]
+        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(List<BookResponse>))]
         public IActionResult Get()
         {
             var result = _bookStorageService.GetAllBook();
@@ -21,6 +30,7 @@ namespace BookStorage.API.Controllers
 
         // GET api/<StorageController>/5
         [HttpGet("{id}")]
+        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(BookResponse))]
         public IActionResult Get(Guid id)
         {
             var result = _bookStorageService.GetBook(id);
@@ -29,6 +39,7 @@ namespace BookStorage.API.Controllers
 
         // POST api/<StorageController>
         [HttpPost]
+        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(BookResponse))]
         public IActionResult Post(BookRequest book)
         {
             var result = _bookStorageService.AddBook(book);
@@ -37,6 +48,7 @@ namespace BookStorage.API.Controllers
 
         // PUT api/<StorageController>/5
         [HttpPut("{id}")]
+        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(BookResponse))]
         public IActionResult Put(Guid id, BookRequest book)
         {
             var result = _bookStorageService.UpdateBook(id, book);
@@ -45,6 +57,7 @@ namespace BookStorage.API.Controllers
 
         // DELETE api/<StorageController>/5
         [HttpDelete("{id}")]
+        [SwaggerResponse((int)HttpStatusCode.OK)]
         public IActionResult Delete(Guid id)
         {
             _bookStorageService.DeleteBook(id);
